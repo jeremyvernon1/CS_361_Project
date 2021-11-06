fb9b8510477ab660885fdaf10feadd1bc29cec74
jeremyvernon1 <jeremyvernon@icloud.com>
2021-11-06 16:47:17 -0400

    Refactored to be more effecient

--- public/js/currConvert.js
+++ public/js/currConvert.js
@@ -1,8 +1,7 @@
 const puppeteer = require('puppeteer');
 const receiveAmount = 1;
-const receiveCurrency = "VENEZUELA";
-var conversionFactor = 0;
-const countryArr = [];
+const receiveCountry = "SOUTH AFRICA";
+var found = false;
 
 // Class for country, rate, and currency type
 class Rates {
@@ -27,15 +26,9 @@ async function scrapeProduct(url) {
 
     // Get info
     for (let i = 1; i < 24; i++) {
-        // Get converstion rate
-        var elRateUrl = '//*[@id="content"]/table/tbody/tr[' + i + ']/td[2]';
-        var [el] = await page.$x(elRateUrl);
-        var txt = await el.getProperty('innerText');
-        var rate = await txt.jsonValue();
-        
         // Get country
-        var elcountryUrl = 'html/body/div[3]/table/tbody/tr[' + i + ']/th';
-        [el] = await page.$x(elcountryUrl);
+        var elCountryUrl = 'html/body/div[3]/table/tbody/tr[' + i + ']/th';
+        [el] = await page.$x(elCountryUrl);
         txt = await el.getProperty('innerText');
         country = await txt.jsonValue();
         /* Remove astericks. Used from stackoverflow.com/questions/4564414/
@@ -43,35 +36,38 @@ async function scrapeProduct(url) {
         while(country.charAt(0) == '*') {
             country = country.substring(1);
         }
-        country = properCase(country);
 
-        // Get country currency
-        var elcurrencyUrl = '//*[@id="content"]/table/tbody/tr[' + i + ']/td[1]';
-        [el] = await page.$x(elcurrencyUrl);
-        txt = await el.getProperty('innerText');
-        currency = await txt.jsonValue();
-        // Pluralize and format name of currency
-        if (rate != 1) {
-            currency = currency + "s";
+        if (receiveCountry.toLowerCase() == country.toLowerCase()) {
+            found = true;
+            country = properCase(country);
+
+            // Get converstion rate
+            var elRateUrl = '//*[@id="content"]/table/tbody/tr[' + i + ']/td[2]';
+            [el] = await page.$x(elRateUrl);
+            txt = await el.getProperty('innerText');
+            var rate = Number(await txt.jsonValue());
+            
+            // Get country currency
+            var elcurrencyUrl = '//*[@id="content"]/table/tbody/tr[' + i + ']/td[1]';
+            [el] = await page.$x(elcurrencyUrl);
+            txt = await el.getProperty('innerText');
+            var currency = await txt.jsonValue();
+            // Pluralize and format name of currency
+            if (rate != 1) {
+                currency = currency + "s";
+            }
+            currency = properCase(currency);
+
+            // Convert
+            const returnCurrAmount = receiveAmount * rate;
+
+            // Display
+            console.log("\n", returnCurrAmount, currency, "\n");
         }
-        currency = properCase(currency);
-
-        // Save results in array
-        countryArr[i - 1] = new Rates(country, Number(rate), currency)
-        //rates.set(country.toLowerCase(), Number(rate));
     }
-    
-    // 
-    for (i = 0; i < (countryArr.length); i++) {
-        if (receiveCurrency.toLowerCase() == countryArr[i].countryName.toLowerCase()) {
-                // Convert
-                conversionFactor = countryArr[i].rate;
-                const returnCurrAmount = receiveAmount * conversionFactor;
-                const returnCurrType = countryArr[i].currency;
 
-                // Display
-                console.log("\n", returnCurrAmount, returnCurrType, "\n");
-        }
+    if (!found) {
+        console.log("\nSorry. Country not found.\n");
     }
 
     // End session
