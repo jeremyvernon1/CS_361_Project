46fee0fcb647c7868be66c49791a168d3709c5f9
jeremyvernon1 <jeremyvernon@icloud.com>
2021-11-06 10:42:22 -0400

    Getting country and rate

--- public/js/currConvert.js
+++ public/js/currConvert.js
@@ -1,43 +1,52 @@
 const puppeteer = require('puppeteer');
-const receiveCurrency = 0;
-const returnCurrency = 0;
+const receiveCurrency = 1;
 var conversionFactor = 0;
 
 async function scrapeProduct(url) {
+    // Initialize scraper
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
 
-    const [el0] = await page.$x('//*[@id="imgBlkFront"]');
-    const src = await el0.getProperty('src');
-    const imgUrl = await src.jsonValue();
-   
-    const [el1] = await page.$x('//*[@id="productTitle"]');
-    const txt0 = await el1.getProperty('textContent');
-    const title = await txt0.jsonValue();
-   
-    const [el2] = await page.$x('//*[@id="corePrice_feature_div"]/div/span/span[2]');
-    const txt1 = await el2.getProperty('textContent');
-    const price = await txt1.jsonValue();
+    let rates = new Map();
 
-    console.log({imgUrl, title, price});
+    // Get info
+    for (let i = 1; i < 24; i++) {
+        // Get converstion rate
+        var elRateUrl = '//*[@id="content"]/table/tbody/tr[' + i + ']/td[2]';
+        var [el] = await page.$x(elRateUrl);
+        var txt = await el.getProperty('innerText');
+        var rate = await txt.jsonValue();
+        
+        // Get country
+        var elcountryUrl = 'html/body/div[3]/table/tbody/tr[' + i + ']/th';
+        [el] = await page.$x(elcountryUrl);
+        txt = await el.getProperty('innerText');
+        var country = await txt.jsonValue();
+        // Remove astericks
+        /* Used from stackoverflow.com/questions/4564414/
+        delete-first-character-of-a-string-in-javascript */
+        while(country.charAt(0) == '*') {
+            country = country.substring(1);
+        }
 
+        // Save results in map
+        rates.set(country, Number(rate));
+    }
+    
+    // Convert
+    /* conversionFactor = prices[0];
+    const returnCurrency = receiveCurrency * conversionFactor; */
+
+    // Display
+    /* for (let j = 0; j < 23; j++) {
+        console.log(j, rates.get(j));
+    } */
+    console.log("\nAustralia: ", rates.get("AUSTRALIA"), "\n");
+
+    // Close
     browser.close();
 }
 
-scrapeProduct('https://smile.amazon.com/Black-Swan-Impact-Improbable-Incerto/dp/1400063515/ref=sr_1_2?crid=39DIQJF7GMN8V&keywords=black+swan+book&qid=1636200890&qsid=137-3588002-2554333&sprefix=black+swan+book%2Caps%2C148&sr=8-2&sres=081297381X%2C1400063515%2C0967375517%2CB081QQMG6Y%2CB08PJWKW4W%2CB00HEU3P8S%2CB07YGNX1HQ%2CB07MF4VTXN%2CB01APIXTJ0%2CB00BH68UCG%2CB01EXOKBZW%2C0778311074%2C0399590455%2CB075L5J1K2%2C1737419807%2C0312428243&srpt=ABIS_BOOK');
-
-
-// Conversion Rates
-conversionFactor = 0.5;
-
-// Convert input into digit
-
-
-// Convert into new currency
-if (!receiveCurrency) {
-    console.log("\ninput currency not received.\n");
-}
-else {
-    returnCurrency = receiveCurrency * conversionFactor;
-}
+// Run scraper
+scrapeProduct('https://www.federalreserve.gov/releases/h10/current/');
